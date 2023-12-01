60cbaa5af33c4125972378da4851aa2dc0661af5
nghiack7 <ngocnghia128@gmail.com>
2023-12-01 11:49:53 +0700

    update ui

--- views/src/components/Home/Home.js
+++ views/src/components/Home/Home.js
@@ -1,21 +1,21 @@
 const Home = () => {
   return (
     <section>
-      <h1>Welcome to Manage App!</h1>
+      <h1>Chào Mừng Bạn Đến Với Ứng Dụng Quản Lý Bán Hàng!</h1>
       <div className="body-content">
         <p>
-          Manage App is your all-in-one solution for efficient customer, order, and product management.
-          Whether you're handling customer relationships, processing orders, or managing your product inventory,
-          our application simplifies the process, providing you with the tools needed to streamline your business operations.
+          Ứng dụng Quản lý là giải pháp toàn diện của bạn cho việc quản lý khách hàng, đơn hàng và sản phẩm một cách hiệu quả.
+          Cho dù bạn đang xử lý mối quan hệ khách hàng, xử lý đơn hàng, hoặc quản lý hàng tồn kho của bạn,
+          ứng dụng của chúng tôi đơn giản hóa quá trình đó, cung cấp cho bạn các công cụ cần thiết để tối ưu hóa hoạt động kinh doanh của bạn.
         </p>
         <p>
-          With an intuitive interface and robust functionalities, our app empowers you to create, update, and track
-          customer profiles, orders, and products effortlessly. Dive into detailed analytics, stay on top of inventory,
-          and gain valuable insights to drive business growth.
+          Với giao diện trực quan và các chức năng mạnh mẽ, ứng dụng của chúng tôi giúp bạn tạo, cập nhật và theo dõi
+          hồ sơ khách hàng, đơn hàng và sản phẩm một cách dễ dàng. Đào sâu vào phân tích chi tiết, theo dõi hàng tồn kho,
+          và có được thông tin chi tiết để thúc đẩy sự phát triển kinh doanh.
         </p>
         <p>
-          Get started today and experience the convenience of unified customer, order, and product management
-          at your fingertips with Manage App.
+          Bắt đầu ngay hôm nay và trải nghiệm sự tiện lợi của việc quản lý khách hàng, đơn hàng và sản phẩm được tổ chức
+          tại đầu ngón tay bạn với Ứng dụng Quản lý.
         </p>
       </div>
     </section>
--- views/src/components/Layout/NavigationBar.js
+++ views/src/components/Layout/NavigationBar.js
@@ -1,15 +1,15 @@
-import { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 const NavigationBar = () => {
   const history = useHistory();
 
   const isLoggedIn = true; // Fetch authentication status from context
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <button
-        type="button"
         className="navbar-toggler"
+        type="button"
         data-toggle="collapse"
         data-target="#navbarCollapse"
         aria-controls="navbarCollapse"
@@ -18,39 +18,33 @@ const NavigationBar = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div
-        className="collapse navbar-collapse justify-content-between"
-        id="navbarCollapse"
-      >
-        <div className="d-flex flex-row">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Trang Chủ
-              </Link>
-            </li>
-            {isLoggedIn && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/customers">
-                    Danh Sách Khách Hàng
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/products">
-                    Sản Phẩm Của Tôi
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/orders">
-                    Đơn Hàng Đã Bán
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-          
-        </div>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Trang Chủ
+            </Link>
+          </li>
+          {isLoggedIn && (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link" to="/customers">
+                  Danh Sách Khách Hàng
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  Sản Phẩm Của Tôi
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/orders">
+                  Đơn Hàng Đã Bán
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </nav>
   );
